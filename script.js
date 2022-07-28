ef5b551efac13cb051ad087521dbb1f1b026efa8
Zebaginas <j.gecevicius@lancaster.ac.uk>
2022-07-28 16:48:18 +0100

    finished up the Admin Dashboard project will need to addres the side nav not sizing properly and the side information area not sizing properly

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let companyLogo = document.querySelector("img");
 let divs = document.querySelectorAll(".noDisplay");
+let sideNav = document.querySelector(".sideNav")
 const delay = 0.7;
 
 companyLogo.addEventListener("click", () => {
+  sideNav.classList.toggle("minWidth");
   companyLogo.style.transition = `${delay}s`;
   companyLogo.style.transform = "rotate(360deg)";
 divs.forEach((elem) => elem.classList.toggle("noDisplay"));
